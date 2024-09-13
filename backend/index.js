7c73d39f24fea335cba6752dd68e9e6a8c39aadc
Papun1111 <gohanmohapatra@gmail.com>
2024-09-13 17:00:50 +0530

    controller for signup

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express=require ("express");
 const mongoose=require('mongoose');
 const bodyParser = require("body-parser");
 const cors = require("cors");
-import User from './model/userModels';
+const {User} = require('./model/userModels');
 const PORT=process.env.PORT || 3000;
 const url=process.env.MONGO_URL;
 const app=express();
@@ -43,23 +43,22 @@ app.post('/api/signup', async (req, res) => {
   }
 
   try {
-    // Check if the user already exists
+  
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    // Hash the password before saving it
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Create a new user instance with the hashed password
+
     const newUser = new User({ name, email, password: hashedPassword });
 
    
     await newUser.save();
 
-    // Return success message
+   
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
     console.error('Signup Error:', error);
